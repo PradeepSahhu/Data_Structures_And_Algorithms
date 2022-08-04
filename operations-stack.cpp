c8b29a670b12cd68e1409727121a9e743037c704
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-08-04 07:19:27 +0530

    updated stack operations

--- operations-stack.cpp
+++ operations-stack.cpp
@@ -27,7 +27,7 @@ int isFull(struct stack *ptr){
 
 void pushStack(struct stack * ptr, int value){
     if(isFull(ptr)){
-        cout<<"The stack overflow"<<endl;
+        printf("The stack overflow! cannot push %d to the stack \n", value);
     }else{
         ptr->top++;
         ptr->arr[ptr->top] = value;
@@ -36,7 +36,7 @@ void pushStack(struct stack * ptr, int value){
 
 int popStack(struct stack *ptr){
     if(isEmpty(ptr)){
-        cout<<"The stack is Empty pleas push before pop"<<endl;
+        cout<<"The stack is Underflow! please push before pop"<<endl;
         return -1;
     }else{
         int value = ptr->arr[ptr->top];
@@ -48,29 +48,29 @@ int popStack(struct stack *ptr){
 
 int main(){
 
-    struct stack *sp; //stack pointer
+    struct stack *sp; //Structure pointer
     sp = (struct stack *) malloc (sizeof(struct stack));
     sp->size = 3;
     sp->top = -1;
     sp->arr = (int *) malloc (sp->size * sizeof(int));
 
-    // Manually pushing into the array..
+    // *********************** Manually pushing into the array. ****************************
     // sp->arr[0] = 3;
     // sp->top++;
 
-    // Performing push and pop operations on list.
+    // ***************** Performing push and pop operations on list.*********************
 
     pushStack(sp, 400);
     pushStack(sp, 10);
-    pushStack(sp, 600); //---> only 3 pushes in the stack. at this point stack is full 
-    // pushStack(sp, 100); // Stack overflow
+    pushStack(sp, 600); //---> only 3 pushed in the stack. at this point stack is full 
+    pushStack(sp, 69); // Stack overflow since stack is full.
 
-    cout<<popStack(sp)<<endl;
-    cout<<popStack(sp)<<endl;
-    cout<<popStack(sp)<<endl;
-    // cout<<popStack(sp)<<endl; // The stack is empty at this point so no other element will be popped out.
+    // cout<<popStack(sp)<<" is popped out<<endl;
+    // cout<<popStack(sp)<<" is popped out<<endl;
+    // cout<<popStack(sp)<<" is popped out<<endl;
+    // cout<<popStack(sp)<<" is popped out<<endl; // The stack is empty at this point so no other element will be popped out.
 
-    // checking if the stack is empty and full or not.
+    // ******************************** checking if the stack is empty and full or not. ********************
     if(isEmpty(sp)){
         cout<<"The Stack is Empty"<<endl;
     }else{
