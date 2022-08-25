a44f31d44219cf61586cdea7b8218d6e90a62ffb
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-08-25 23:49:10 +0530

    Queue and Double Ended Queue

---
+++ true.cpp
@@ -0,0 +1,24 @@
+// Custom boilerplate: using configure user's snippet in the manage.
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+   
+    // int i = 1;
+    // while(true) {
+    //     cout<<"hello India "<<i<<endl;
+    //     i++;
+
+    //     if(i == 35){
+    //         break;
+    //     }
+    // }
+
+    char name[] = "myNameIsPradeepSahu";
+    printf("the length of name array is %d", strlen(name));
+    // strlen() is used to print the length of the string or the array.
+
+    return 0;
+}
